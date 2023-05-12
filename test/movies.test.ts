f94db6477af9507827a093379cf1b708496f2127
stefdev <sstefdev@gmail.com>
2023-05-12 12:41:24 +0200

    added new integration tests to movie api

--- test/movies.test.ts
+++ test/movies.test.ts
@@ -1,66 +1,56 @@
 import { getMovies, getMovieById, getMovieQuotes } from "../src/api/movie";
-import { APIResponse } from "../index";
+import { APIResponse, Movie, Quote } from "../index";
 
-describe("getMovies", () => {
-  let response: APIResponse;
+describe("getMovies Integration Tests", () => {
+  let movies: APIResponse | null;
 
   beforeAll(async () => {
-    response = await getMovies();
+    movies = await getMovies();
   });
 
-  it("should return an array of movies", async () => {
-    expect(Array.isArray(response.docs)).toBe(true);
+  it("should fetch movies successfully", async () => {
+    expect(movies).not.toBeNull();
+    if (movies) {
+      expect(Array.isArray(movies.docs)).toBe(true);
+    }
   });
 
-  it("should return movies with required properties", async () => {
-    response.docs.forEach((movie) => {
-      expect(movie).toHaveProperty("_id");
-      expect(movie).toHaveProperty("name");
-      expect(movie).toHaveProperty("runtimeInMinutes");
-      expect(movie).toHaveProperty("budgetInMillions");
-      expect(movie).toHaveProperty("boxOfficeRevenueInMillions");
-      expect(movie).toHaveProperty("academyAwardNominations");
-      expect(movie).toHaveProperty("academyAwardWins");
-      expect(movie).toHaveProperty("rottenTomatoesScore");
-    });
+  it("should fetch movies with pagination", async () => {
+    expect(movies).not.toBeNull();
+    if (movies) {
+      const params = {
+        page: 1,
+        limit: 10,
+      };
+      const paginatedMovies = await getMovies(params);
+      expect(paginatedMovies).not.toBeNull();
+      if (paginatedMovies) {
+        expect(Array.isArray(paginatedMovies.docs)).toBe(true);
+        expect(paginatedMovies.docs.length).toBeGreaterThan(0);
+        expect(paginatedMovies.page).toBe(params.page);
+        expect(paginatedMovies.limit).toBe(params.limit);
+      }
+    }
   });
 
-  it("should return paginated results with default values", async () => {
-    expect(response).toHaveProperty("docs");
-    expect(Array.isArray(response.docs)).toBe(true);
-    expect(response).toHaveProperty("total");
-    expect(typeof response.total).toBe("number");
-    expect(response).toHaveProperty("limit");
-    expect(typeof response.limit).toBe("number");
-    expect(response).toHaveProperty("offset");
-    expect(typeof response.offset).toBe("number");
-    expect(response).toHaveProperty("page");
-    expect(typeof response.page).toBe("number");
-    expect(response).toHaveProperty("pages");
-    expect(typeof response.pages).toBe("number");
-  });
-
-  it("should return paginated results with custom values", async () => {
-    const customResponse = await getMovies({
-      page: 2,
-      limit: 10,
-    });
-    expect(customResponse.page).toBe(2);
-    expect(customResponse.limit).toBe(10);
-  });
-
-  it("should return filtered results based on provided filters", async () => {
-    const customResponse = await getMovies({
-      filters: {
-        match: { name: "The Hobbit Series" },
-      },
-    });
-    const movies = customResponse.docs;
-    expect(Array.isArray(movies)).toBe(true);
-    expect(movies.length).toBeGreaterThan(0);
-    movies.forEach((movie) => {
-      expect(movie.name).toContain("The Hobbit Series");
-    });
+  it("should fetch movies with filters", async () => {
+    expect(movies).not.toBeNull();
+    if (movies) {
+      const params = {
+        filters: {
+          match: { name: "The Hobbit Series" },
+        },
+      };
+      const filteredMovies = await getMovies(params);
+      expect(filteredMovies).not.toBeNull();
+      if (filteredMovies) {
+        expect(Array.isArray(filteredMovies.docs)).toBe(true);
+        expect(filteredMovies.docs.length).toBeGreaterThan(0);
+        filteredMovies.docs.forEach((movie: Movie | Quote) => {
+          expect((movie as Movie).name).toContain("The Hobbit Series");
+        });
+      }
+    }
   });
 });
 
@@ -68,40 +58,36 @@ describe("getMovieById", () => {
   it("should return the correct movie object for a valid movie ID", async () => {
     const movieId = "5cd95395de30eff6ebccde5c";
     const movie = await getMovieById(movieId);
-    expect(movie).not.toBeNull();
-    expect(movie?.docs[0]._id).toBe(movieId);
+    if (movie) {
+      expect(movie).not.toBeNull();
+      expect(movie?.docs[0]._id).toBe(movieId);
+    }
   });
 
-  it("should return an error with status code 500 for a non-existent movie ID", async () => {
-    const nonExistentId = "00000";
-    try {
-      await getMovieById(nonExistentId);
-    } catch (error) {
-      expect(error).toHaveProperty("response");
-      expect(error.response).toHaveProperty("status");
-      expect(error.response.status).toBe(500);
-    }
+  it("should return null for an invalid movie ID", async () => {
+    const invalidMovieId = "000000";
+    const movie = await getMovieById(invalidMovieId);
+    expect(movie).toBeNull();
   });
 });
 
 describe("getMovieQuotes", () => {
   it("should return an array of quotes for a specific movie", async () => {
-    const movieId = "5cd95395de30eff6ebccde56";
-    const { docs } = await getMovieQuotes(movieId);
-    expect(Array.isArray(docs)).toBe(true);
+    const movieId = "5cd95395de30eff6ebccde5c";
+    const quotes = await getMovieQuotes(movieId);
+    expect(Array.isArray(quotes.docs)).toBe(true);
+    expect(quotes.docs.length).toBeGreaterThan(0);
   });
 
-  it("should return an empty array if the movie ID does not exist", async () => {
+  it("should throw an error if the movie ID does not exist", async () => {
     const invalidMovieId = "000000";
-    const quotes = await getMovieQuotes(invalidMovieId);
-    expect(Array.isArray(quotes)).toBe(true);
-    expect(quotes).toHaveLength(0);
+    await expect(getMovieQuotes(invalidMovieId)).rejects.toThrowError();
   });
 
   it("should return an empty array if the movie has no quotes", async () => {
-    const movieId = "5cd95395de30eff6ebccde56";
-    const { docs } = await getMovieQuotes(movieId);
-    expect(Array.isArray(docs)).toBe(true);
-    expect(docs).toHaveLength(0);
+    const movieId = "5cd95395de30eff6ebccde59";
+    const quotes = await getMovieQuotes(movieId);
+    expect(Array.isArray(quotes.docs)).toBe(true);
+    expect(quotes.docs.length).toBe(0);
   });
 });
